f95fcb954f7b5fb8c1fe401194630fd594bfd60e
Zcyyyyyy <chenyu@cca.edu>
2022-12-12 07:14:42 -0800

    final

---
+++ test/index.js
@@ -0,0 +1,40 @@
+// Timercount down
+let timeSecond = 900;
+const timeH = document.querySelector(".timer");
+
+displayTime(timeSecond);
+
+const countDown = setInterval(() => {
+  timeSecond--;
+  displayTime(timeSecond);
+  if (timeSecond == 0 || timeSecond < 1) {
+    endCount();
+    clearInterval(countDown);
+  }
+}, 1000);
+
+function displayTime(second) {
+  const min = Math.floor(second / 60);
+  const sec = Math.floor(second % 60);
+  timeH.innerHTML = `
+  ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
+  `;
+}
+
+function endCount() {
+  timeH.innerHTML = "Time out";
+}
+// *********************
+// This Code is for only the floating card in right bottom corner
+// **********************
+
+const touchButton = document.querySelector(".float-text");
+const card = document.querySelector(".float-card-info");
+const close = document.querySelector(".gg-close-r");
+
+touchButton.addEventListener("click", moveCard);
+close.addEventListener("click", moveCard);
+
+function moveCard() {
+  card.classList.toggle("active");
+}
---
+++ test2/index.js
@@ -0,0 +1,53 @@
+function count(activeIndex) {
+  if (window.timer) {
+    clearInterval(window.timer);
+  }
+  // 配置从多少时间倒计时
+  let timeSecond = 900;
+  let timeH = document.getElementsByClassName("timer");
+  if (!timeH[Number(activeIndex)]) {
+    return;
+  }
+  timeH = timeH[Number(activeIndex)];
+  displayTime(timeSecond);
+  window.timer = setInterval(() => {
+    timeSecond--;
+    displayTime(timeSecond);
+    if (timeSecond == 0 || timeSecond < 1) {
+      endCount();
+      clearInterval(window.timer);
+    }
+  }, 1000);
+
+  function displayTime(second) {
+    const min = Math.floor(second / 60);
+    const sec = Math.floor(second % 60);
+    timeH.innerHTML = `
+    ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
+    `;
+  }
+
+  function endCount() {
+    timeH.innerHTML = "Time out";
+  }
+}
+var mySwiper = new Swiper('.swiper', {
+  autoplay: {
+    // 15分钟为：1000 * 60 *15
+    delay: 5000,
+    stopOnLastSlide: true,
+    disableOnInteraction: false,
+  },
+  loop: false,
+  direction: 'vertical',
+  effect: 'creative', //特效
+  noSwiping: true,
+  on: {
+    init: function () {
+      count(this.activeIndex)
+    },
+    slideChangeTransitionEnd: function () {
+      count(this.activeIndex)
+    },
+  },
+})
